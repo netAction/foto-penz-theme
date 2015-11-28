5dbe96664bb10a06aafafaf16a8072adcb5b538e
Thomas Schmidt <schmidt@netaction.de>
2015-11-28 12:28:51 +0100

    Favicon und Title-Fix

--- templates/head.php
+++ templates/head.php
@@ -1,6 +1,9 @@
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <?php wp_head(); ?>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="shortcut icon" href="<?php echo get_template_directory_uri() ?>/assets/img/favicon.ico" type="image/x-icon" />
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo get_template_directory_uri() ?>/assets/img/apple-touch-icon-114x114-precomposed.png" />
+	<?php wp_head(); ?>
 </head>
--- templates/page-header.php
+++ templates/page-header.php
@@ -1,5 +1,7 @@
 <?php use Roots\Sage\Titles; ?>
 
+<?php if (Titles\title()) { ?>
 <div class="page-header">
 	<h2><span><?= Titles\title(); ?></span></h2>
 </div>
+<?php } ?>
