aa69c0fda34490eb8eb771e6ca1ef9a4ada8b4c9
Thomas Schmidt <schmidt@netaction.de>
2016-02-21 12:04:53 +0100

    Minor Gallery Fix

--- templates/widget-slider-gallery.php
+++ templates/widget-slider-gallery.php
@@ -1,12 +1,12 @@
 <div class="widget-slider-gallery row">
-<div class="col-sm-6 col-md-7">
+<div class="col-sm-6 col-md-6">
 <div class="widget-richtext">
 <?php
 	the_sub_field('richtext');
 ?>
 </div><!-- widget-richtext -->
 </div>
-<div class="col-sm-6 col-md-3">
+<div class="col-sm-6 col-md-6">
 <?php
 	$image_ids = get_sub_field('gallery', false);
 	$shortcode = '[gallery-slider ids="' . implode(',', $image_ids) . '"]';
